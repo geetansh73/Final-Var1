21a9ed14d30e865f7054a59cc24f9c3af453ef3c
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-08-13 22:21:25 +0530

    Create f.java

---
+++ f.java
@@ -0,0 +1,20 @@
+class sample
+{
+final int t=5;
+void display()
+{
+final int c;
+{
+c=10;
+}
+System.out.print(c);
+}
+}
+class finalvar1
+{
+public static void main(String geetu[])
+{
+sample t=new sample();
+t.display();
+}
+}
